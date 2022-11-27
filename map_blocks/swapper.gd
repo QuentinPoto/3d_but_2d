491bcec1d7ab5e9222658d70f28ec7352703d0f9
Quentin <quentin.jungo@gmail.com>
2022-11-27 14:50:32 +0100

    collision marche en z et x

--- map_blocks/swapper.gd
+++ map_blocks/swapper.gd
@@ -1,6 +1,8 @@
 class_name Swapper
-extends MeshInstance
+extends StaticBody
 
 func _on_Player_player_action(player_pos: Vector3):
+	print("signal received")
 	if round(translation.x) == round(player_pos.x) and round(translation.z) == round(player_pos.z):
+		print("player is on me")
 		SwapLogic.on_reverse_map()
--- player/player.gd
+++ player/player.gd
@@ -4,9 +4,10 @@ class_name Player
 signal player_action
 
 export var speed: float = 3
-
 var player_swapped: bool # Make private ??
 var _velocity: Vector3
+
+# TODO pour que les collision fonctionnent avec la gridmap, il faut ajouter une shape a chaque cube dans la mesh library
 
 func _input(event):
 	if event.is_action_pressed("on_action"):
@@ -14,37 +15,47 @@ func _input(event):
 
 func _physics_process(delta):
 	if not SwapLogic.is_swapping:
-		_player_movement(delta)
+		_player_movement()
 		player_swapped = false
 	else:
 		_player_swapper()
 
+func _on_floor(new_pos: Vector3) -> bool:
+	var ray = $RayCast
+	print(ray.is_colliding())
+	return ray.is_colliding()
+	"""
+	var _floor: GridMap = get_node("../GridMap")
+	var floor_pos = _floor.world_to_map(new_pos.round())
+	var floor_tile = _floor.get_cell_item(floor_pos.x, floor_pos.y, floor_pos.z)
+	print("new_pos ", new_pos,"floor_pos ", floor_pos, "floor_t ", floor_tile)
+	return floor_tile != -1
+	"""
+
 ############ PLAYER MOVEMENT #############
-func _player_movement(delta):
+func _player_movement():
+	_on_floor(translation)
 	var direction = Vector3.ZERO
-	if Input.is_action_pressed("ui_down"):
-		direction.x -= 1
-		direction.z += 1
-	if Input.is_action_pressed("ui_up"):
-		direction.x += 1
-		direction.z -= 1
+	if Input.is_action_pressed("ui_right"):
+		direction += Vector3(1, 0, 1)
+	if Input.is_action_pressed("ui_left"):
+		direction -= Vector3(1, 0, 1)
 	if not SwapLogic.is_player_down:
-		if Input.is_action_pressed("ui_right"):
-			direction += Vector3(1, 0, 1)
-		if Input.is_action_pressed("ui_left"):
-			direction -= Vector3(1, 0, 1)
+		if Input.is_action_pressed("ui_down"):
+			direction += Vector3(-1, 0, 1)
+		if Input.is_action_pressed("ui_up"):
+			direction += Vector3(1, 0, -1)
 	else:
-		if Input.is_action_pressed("ui_right"):
-			direction -= Vector3(1, 0, 1)
-		if Input.is_action_pressed("ui_left"):
-			direction += Vector3(1, 0, 1)
-		pass
+		if Input.is_action_pressed("ui_down"):
+			direction -= Vector3(-1, 0, 1)
+		if Input.is_action_pressed("ui_up"):
+			direction -= Vector3(1, 0, -1)
 	direction = direction.normalized() # permet d'eviter que ca aie 2x plus vite en diagonale
 	_velocity.x = direction.x * speed
 	_velocity.z = direction.z * speed
 	_velocity.y = 0 # TODO a enlever en cas d'echelle
+	#if _on_floor(translation + _velocity * Vector3.UP): # TODO comment empecher de tomber ??
 	_velocity = move_and_slide(_velocity, Vector3.UP)
-
 
 ##########	PLAYER SWAPPER	##########
 const swap_begin: int = 45
@@ -64,17 +75,13 @@ func _swap_player(random: bool):
 	#translation.y = -(translation.y)
 	var block_size = 2
 	translation.y = translation.y + block_size if SwapLogic.is_player_down else translation.y - block_size
-	rotation_degrees.z = 0 if rotation_degrees.z == 180 else 180
+	$Sprite.rotation_degrees.z = 0 if $Sprite.rotation_degrees.z == 180 else 180
 	player_swapped = !player_swapped
 	SwapLogic.is_player_down = !SwapLogic.is_player_down
 
 
-
-
-
-
+########### PLAYER FX (BLINKING)
 """
-
  -> effet de clignotement. Vu que le swap player creer le meme effet, pas besoin de mettre les deux...
 func blincking():
 	var blincking_speed: int = round(2 * abs(SwapLogic.swapping_p - 50) / 10)
